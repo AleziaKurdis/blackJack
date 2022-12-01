cbd95bf5fca771312bc3e607195945d83e84bb42
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2022-11-30 23:08:36 -0500

    Add files via upload

--- displayer.js
+++ displayer.js
@@ -66,7 +66,7 @@
             "parentID": thisEntityID,
             "renderWithZones": thisRenderWithZones,
             "localPosition": cardHandlerPosition[playerNo].localPosition,
-            "localRotation": Quat.fromVec3Degrees({"x": 0, "y": cardHandlerPosition[playerNo].rotation, "z": 0}),
+            "localRotation": Quat.fromVec3Degrees({"x": 90, "y": cardHandlerPosition[playerNo].rotation, "z": 90}),
             "type": "Shape",
             "shape": "Cube",
             "dimensions": {"x": 0.01, "y": 0.01, "z": 0.01},
