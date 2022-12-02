243897babb494fec291585f37a6784dbc1e78ba3
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2022-12-01 21:25:21 -0500

    Add files via upload

--- displayer.js
+++ displayer.js
@@ -49,11 +49,11 @@
     var playersCardsIDs = [Uuid.NULL, Uuid.NULL, Uuid.NULL, Uuid.NULL, Uuid.NULL];
     
     var cardHandlerPosition = [
-        {"localPosition": {"x": 0.1343, "y": 1.1464, "z": -0.4646}, "rotation": -90}, //croupier
-        {"localPosition": {"x": 0.7583, "y": 1.1464, "z": -0.4363}, "rotation": -144}, //player 1
-        {"localPosition": {"x": 0.3689, "y": 1.1464, "z": -0.8022}, "rotation": -108}, //player 2 
-        {"localPosition": {"x": -0.1467, "y": 1.1464, "z": -0.8686}, "rotation": -72}, //player 3
-        {"localPosition": {"x": -0.6194, "y": 1.1464, "z": -0.6026}, "rotation": -36}, //player 4
+        {"localPosition": {"x": 0.1343, "y": 1.0664, "z": -0.4646}, "rotation": -90}, //croupier
+        {"localPosition": {"x": 0.7583, "y": 1.0664, "z": -0.4363}, "rotation": -144}, //player 1
+        {"localPosition": {"x": 0.3689, "y": 1.0664, "z": -0.8022}, "rotation": -108}, //player 2 
+        {"localPosition": {"x": -0.1467, "y": 1.0664, "z": -0.8686}, "rotation": -72}, //player 3
+        {"localPosition": {"x": -0.6194, "y": 1.0664, "z": -0.6026}, "rotation": -36}, //player 4
     ];
 
     function displayHand(playerNo, handArray) {
