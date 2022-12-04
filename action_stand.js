e4776fcf1f80d8762247bcc960258e237b0c48f8
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2022-12-04 16:48:34 -0500

    Add files via upload

---
+++ action_stand.js
@@ -0,0 +1,71 @@
+"use strict";
+//
+//  action_stand.js
+//
+//  Created by Alezia Kurdis, December 4th, 2022.
+//  Copyright 2022 Alezia Kurdis.
+//
+//  Black Jack Game for Overte - trigger to stand.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function(){
+    var jsMainFileName = "action_stand.js";
+    var ROOT = Script.resolvePath('').split(jsMainFileName)[0];    
+    var channelComm = "ak.blackJack.ac.communication"; 
+    var oneTimeOnly = false;
+   
+    function pay(entityID) {
+        if (oneTimeOnly === false) {           
+            var message = {
+                "action": "ACTION_STAND",
+                "avatarID": MyAvatar.sessionUUID
+            };
+            Messages.sendMessage(channelComm, JSON.stringify(message));
+
+            oneTimeOnly = true;
+        }
+
+    }; 
+
+    var MAX_CLICKABLE_DISTANCE_M = 2;
+
+    // Constructor
+    var _this = null;
+
+    function clickableUI() {
+        _this = this;
+        this.entityID = null;
+    }
+
+    // Entity methods
+    clickableUI.prototype = {
+        preload: function (id) {
+            _this.entityID = id;
+            HMD.displayModeChanged.connect(this.displayModeChangedCallback);
+        },
+
+        displayModeChangedCallback: function() {
+            if (_this && _this.entityID) {
+                //Nothing
+            }
+        },
+
+        mousePressOnEntity: function (entityID, event) {
+            if (event.isPrimaryButton && 
+                Vec3.distance(MyAvatar.position, Entities.getEntityProperties(_this.entityID, ["position"]).position) <= MAX_CLICKABLE_DISTANCE_M) {
+                    pay(_this.entityID);
+            }
+        },
+
+        unload: function () {
+            HMD.displayModeChanged.disconnect(this.displayModeChangedCallback);
+        }
+    };
+
+    
+    return new clickableUI();
+
+
+})
---
+++ unbetOneToken.js
@@ -0,0 +1,82 @@
+"use strict";
+//
+//  unbetOneToken.js
+//
+//  Created by Alezia Kurdis, December 4th, 2022.
+//  Copyright 2022 Alezia Kurdis.
+//
+//  Black Jack Game for Overte - trigger to remove bet one tokens.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function(){
+    var jsMainFileName = "betOneToken.js";
+    var ROOT = Script.resolvePath('').split(jsMainFileName)[0];    
+    var channelComm = "ak.blackJack.ac.communication"; 
+    var oneTimeOnly = false;
+    var SOUND_COIN = SoundCache.getSound(ROOT + "sounds/coins.wav");
+    
+    function pay(entityID) {
+        if (oneTimeOnly === false) {
+            var amount = parseInt(Entities.getEntityProperties(entityID,["description"]).description, 10);            
+            var message = {
+                "action": "UNBET",
+                "avatarID": MyAvatar.sessionUUID,
+                "amount": amount
+            };
+            Messages.sendMessage(channelComm, JSON.stringify(message));
+            
+            var injectorOptions = {
+                "position": MyAvatar.position,
+                "volume": 0.25,
+                "loop": false,
+                "localOnly": true
+            };
+            var injector = Audio.playSound(SOUND_COIN, injectorOptions);            
+            
+            oneTimeOnly = true;
+        }
+
+    }; 
+
+    var MAX_CLICKABLE_DISTANCE_M = 2;
+
+    // Constructor
+    var _this = null;
+
+    function clickableUI() {
+        _this = this;
+        this.entityID = null;
+    }
+
+    // Entity methods
+    clickableUI.prototype = {
+        preload: function (id) {
+            _this.entityID = id;
+            HMD.displayModeChanged.connect(this.displayModeChangedCallback);
+        },
+
+        displayModeChangedCallback: function() {
+            if (_this && _this.entityID) {
+                //Nothing
+            }
+        },
+
+        mousePressOnEntity: function (entityID, event) {
+            if (event.isPrimaryButton && 
+                Vec3.distance(MyAvatar.position, Entities.getEntityProperties(_this.entityID, ["position"]).position) <= MAX_CLICKABLE_DISTANCE_M) {
+                    pay(_this.entityID);
+            }
+        },
+
+        unload: function () {
+            HMD.displayModeChanged.disconnect(this.displayModeChangedCallback);
+        }
+    };
+
+    
+    return new clickableUI();
+
+
+})
